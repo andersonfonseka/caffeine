e9882cf42f1992de81850dcaa36e592aa9677f73
Fonseca <anderson.fonseca@accenture.com>
2019-04-12 11:19:18 -0300

    '...'

--- caffeine-core-ui/src/test/java/com/andersonfonseka/caffeine/componentes/impl/mock/MockHttpServletResponse.java
+++ caffeine-core-ui/src/test/java/com/andersonfonseka/caffeine/componentes/impl/mock/MockHttpServletResponse.java
@@ -14,13 +14,13 @@ public class MockHttpServletResponse implements HttpServletResponse {
 
 	@Override
 	public String getCharacterEncoding() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String getContentType() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -30,17 +30,17 @@ public class MockHttpServletResponse implements HttpServletResponse {
 			
 			@Override
 			public void write(int b) throws IOException {
-				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
 			public void setWriteListener(WriteListener writeListener) {
-				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
 			public boolean isReady() {
-				// TODO Auto-generated method stub
+				
 				return false;
 			}
 		};
@@ -48,205 +48,205 @@ public class MockHttpServletResponse implements HttpServletResponse {
 
 	@Override
 	public PrintWriter getWriter() throws IOException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void setCharacterEncoding(String charset) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setContentLength(int len) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setContentLengthLong(long len) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setContentType(String type) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setBufferSize(int size) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public int getBufferSize() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public void flushBuffer() throws IOException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void resetBuffer() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public boolean isCommitted() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setLocale(Locale loc) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public Locale getLocale() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void addCookie(Cookie cookie) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public boolean containsHeader(String name) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public String encodeURL(String url) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String encodeRedirectURL(String url) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String encodeUrl(String url) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String encodeRedirectUrl(String url) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void sendError(int sc, String msg) throws IOException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void sendError(int sc) throws IOException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void sendRedirect(String location) throws IOException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setDateHeader(String name, long date) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void addDateHeader(String name, long date) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setHeader(String name, String value) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void addHeader(String name, String value) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setIntHeader(String name, int value) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void addIntHeader(String name, int value) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setStatus(int sc) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void setStatus(int sc, String sm) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public int getStatus() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public String getHeader(String name) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Collection<String> getHeaders(String name) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Collection<String> getHeaderNames() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
